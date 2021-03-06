e6a0a87f26d6eb606d65b02cd7a7fdda9bc78add
IsabelleDeMartin <75711900+IsabelleDeMartin@users.noreply.github.com>
2021-03-06 10:02:41 -0800

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,54 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE "titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar   NOT NULL,
+     PRIMARY KEY ("title_id")
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+     PRIMARY KEY ("dept_no")
+);
+
+CREATE TABLE "employees" (
+    "emp_no" varchar   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar   NOT NULL,
+    "hire_date" date   NOT NULL, 
+	FOREIGN KEY("emp_title_id") REFERENCES "titles" ("title_id"),
+    PRIMARY KEY ("emp_no")
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" varchar   NOT NULL,
+	FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no"),
+	FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no"),
+    PRIMARY KEY ("dept_no","emp_no"));
+	
+CREATE TABLE "dept_emp" (
+    "emp_no" varchar   NOT NULL,
+    "dept_no" varchar   NOT NULL, 
+	FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no"),
+	FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no"),
+   PRIMARY KEY ("emp_no","dept_no")
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" varchar   NOT NULL,
+    "salary" int   NOT NULL, FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no"),
+     PRIMARY KEY ("emp_no")
+);
+
+select * from dept_emp;
+
+commit;
